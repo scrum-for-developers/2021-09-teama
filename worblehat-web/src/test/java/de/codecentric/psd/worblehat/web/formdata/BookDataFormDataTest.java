454177be82c41c890b7592f2e0cfe45774101e17
VOSER Pascal <pascal.voser@hexagon.com>
2021-09-02 16:39:56 +0200

    Add UnitTest for year validation

---
+++ worblehat-web/src/test/java/de/codecentric/psd/worblehat/web/formdata/BookDataFormDataTest.java
@@ -0,0 +1,38 @@
+package de.codecentric.psd.worblehat.web.formdata;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.hasProperty;
+
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BookDataFormDataTest {
+
+  private Validator validator;
+
+  @BeforeEach
+  public void setUp() {
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    validator = factory.getValidator();
+  }
+
+  @Test
+  public void shouldDenyFutureDates() {
+    BookDataFormData bookForm = new BookDataFormData();
+    bookForm.setYearOfPublication("2030");
+    Set<ConstraintViolation<BookDataFormData>> violations =
+        validator.validateProperty(bookForm, "yearOfPublication");
+    assertThat(
+        violations,
+        contains(
+            hasProperty(
+                "messageTemplate", is("{invalid.length.bookDataFormData.yearOfPublication}"))));
+  }
+}
